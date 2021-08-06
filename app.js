c33987dcaf3b991282544d5954ccfb7149db1771
George Bockari <bitwhys@Georges-Mac-mini.local>
2021-08-06 15:22:35 -0400

    feat: converts basic 3.js example to module format

---
+++ app.js
@@ -0,0 +1,64 @@
+import * as THREE from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+
+// Export so that created effects can be used in any other environment/framework
+export default class Sketch {
+  constructor(options) {
+    this.time = 0
+    // TODO: create
+    this.container = options.container
+    this.scene = new THREE.Scene()
+    this.height = this.container.offsetHeight
+    this.width = this.container.offsetWidth
+    this.camera = new THREE.PerspectiveCamera(
+      70,
+      this.width / this.height,
+      0.01,
+      10
+    )
+    this.camera.position.z = 1
+
+    this.renderer = new THREE.WebGLRenderer({ antialias: true })
+    this.renderer.setSize(this.width, this.height)
+    this.container.appendChild(this.renderer.domElement)
+
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement)
+
+    this.resize()
+    this.setupResize()
+    this.addObjects()
+    this.render()
+  }
+
+  setupResize() {
+    window.addEventListener('resize', this.resize.bind(this))
+  }
+
+  resize() {
+    this.height = this.container.offsetHeight
+    this.width = this.container.offsetWidth
+    this.renderer.setSize(this.width, this.height)
+    this.camera.aspect = this.width / this.height
+    this.camera.updateProjectionMatrix()
+  }
+  addObjects() {
+    this.geometry = new THREE.BoxGeometry(0.2, 0.2, 0.2)
+    this.material = new THREE.MeshNormalMaterial()
+
+    this.mesh = new THREE.Mesh(this.geometry, this.material)
+    this.scene.add(this.mesh)
+  }
+  // animation loop
+  render() {
+    this.time += 0.05
+    // console.log(this.time)
+    this.mesh.rotation.x = this.time / 2000
+    this.mesh.rotation.y = this.time / 1000
+    this.renderer.render(this.scene, this.camera)
+    window.requestAnimationFrame(this.render.bind(this))
+  }
+}
+
+new Sketch({
+  container: document.getElementById('wrapper'),
+})
