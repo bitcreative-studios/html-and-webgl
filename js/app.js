34d5a64664fa0f68b5bb2d4180aaf6bd5c089017
George Bockari <bitwhys@Georges-Mac-mini.local>
2021-08-11 07:12:22 -0400

    feat: add basic Three.js setup code

--- js/app.js
+++ js/app.js
@@ -3,42 +3,51 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
 import fragmentShader from './shaders/fragment.glsl'
 import vertexShader from './shaders/vertex.glsl'
 
-import ocean from '../images/ocean-image.jpeg'
-
 // Export so that created effects can be used in any other environment/framework
 export default class Sketch {
   constructor(options) {
-    this.time = 0
-    // TODO: create
-    this.container = options.container
-    this.scene = new THREE.Scene()
+    // this.time = 0
+    const { fov, container, near, far, cameraStart } = options
+
+    /** TODO: store reference to html container element,
+     * container will never be undefined
+     */
+    this.container = container
     this.height = this.container.offsetHeight
     this.width = this.container.offsetWidth
-    this.camera = new THREE.PerspectiveCamera(
-      70,
-      this.width / this.height,
-      0.01,
-      10
-    )
-    this.camera.position.z = 1
 
+    // TODO: create Three.js camera object
+    this.camera = new THREE.PerspectiveCamera(
+      fov || 30,
+      this.width / this.height,
+      1,
+      near || far || 10000
+    )
+    this.camera.position.z = cameraStart || 100
+
+    // TODO: create Three.js scene object
+    this.scene = new THREE.Scene()
+
+    // TODO: create Three.js renderer object
     this.renderer = new THREE.WebGLRenderer({ antialias: true })
     this.renderer.setSize(this.width, this.height)
     this.container.appendChild(this.renderer.domElement)
 
     this.controls = new OrbitControls(this.camera, this.renderer.domElement)
 
+    // initialize
     this.resize()
     this.setupResize()
     this.addObjects()
     this.render()
   }
 
-  setupResize() {
-    window.addEventListener('resize', this.resize.bind(this))
+  setupResize = () => {
+    window.addEventListener('resize', this.resize)
   }
 
-  resize() {
+  // Function to facilitate responsiveness
+  resize = () => {
     this.height = this.container.offsetHeight
     this.width = this.container.offsetWidth
     this.renderer.setSize(this.width, this.height)
@@ -46,37 +55,28 @@ export default class Sketch {
     this.camera.updateProjectionMatrix()
   }
 
-  addObjects() {
-    this.geometry = new THREE.PlaneBufferGeometry(1, 1, 50, 50)
-    // this.material = new THREE.MeshNormalMaterial()
-
-    this.material = new THREE.ShaderMaterial({
-      uniforms: {
-        time: { value: 0 },
-        oceanTexture: { value: new THREE.TextureLoader().load(ocean) },
-      },
-      side: THREE.DoubleSide,
-      fragmentShader,
-      vertexShader,
-      // wireframe: true,
+  addObjects = () => {
+    this.geometry = new THREE.IcosahedronGeometry(20, 4)
+    this.material = new THREE.MeshBasicMaterial({
+      color: 0xb7ff00,
+      wireframe: true,
     })
-
     this.mesh = new THREE.Mesh(this.geometry, this.material)
     this.scene.add(this.mesh)
   }
 
   // animation loop
-  render() {
-    this.time += 0.05
+  render = () => {
+    // this.time += 0.05
     // console.log(this.time)
-    this.mesh.rotation.x = this.time / 2000
-    this.mesh.rotation.y = this.time / 1000
-    this.material.uniforms.time.value = this.time
+    // this.mesh.rotation.x = this.time / 2000
+    // this.mesh.rotation.y = this.time / 1000
+    // this.material.uniforms.time.value = this.time
     this.renderer.render(this.scene, this.camera)
-    window.requestAnimationFrame(this.render.bind(this))
+    window.requestAnimationFrame(this.render)
   }
 }
 
 new Sketch({
-  container: document.getElementById('wrapper'),
+  container: document.getElementById('container'),
 })
